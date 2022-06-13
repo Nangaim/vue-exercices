072e1c467d311cac90b907417c57d28f474be4f6
Nangaim <maignan.thierry@outlook.fr>
2022-06-13 16:27:57 +0200

    feat: form gestion

---
+++ web-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <SignupForm/>
+</template>
+
+<script>
+import SignupForm from './components/SignupForm.vue'
+
+
+export default {
+  name: 'App',
+  components:{
+    SignupForm,
+  }
+}
+</script>
+
+<style>
+body{
+  background: #141414;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
