ad4edbb5045924e1478c1e76ed0296d90c3091e1
Thierry Maignan <maignan.thierry@outlook.fr>
2022-06-13 23:43:33 +0200

    feat: route vue

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ad atque consequatur et repudiandae, laudantium animi eveniet aperiam ducimus ut commodi dolore magni officiis praesentium corporis mollitia? Maxime omnis repellat eligendi suscipit atque nam voluptate beatae, iste assumenda at delectus autem similique quidem nulla veniam quaerat debitis laudantium! Aliquam, quidem.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
