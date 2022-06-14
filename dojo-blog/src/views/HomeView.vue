b0fb8d0480f4f1c736e6a434b8332276df0c3324
Nangaim <maignan.thierry@outlook.fr>
2022-06-14 18:41:14 +0200

    feat: dojo blog

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search : {{search}}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{name}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue'
+export default {
+  name: 'Home',
+  setup() {    
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', "peach"])
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+    return { names, search, matchingNames}
+  },
+}
+</script>
